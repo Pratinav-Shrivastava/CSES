fa2bc564756731b23aa631f1278338174a0deb0c
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2026-02-21 16:39:58 +0530

    Modified Tower_Of_Hanoi

--- Introductory_Problems/Tower_of_Hanoi.py
+++ Introductory_Problems/Tower_of_Hanoi.py
@@ -1,15 +1,18 @@
-def tower_of_hanoi(n, source, target, spare, moves):
+def hanoi(n, start, end, moves):
     if n == 1:
-        moves.append((source, target))
+        moves.append((start, end))
         return
-    tower_of_hanoi(n-1, source, spare, target, moves)
-    moves.append((source, target))
-    tower_of_hanoi(n-1, spare, target, source, moves)
+    else:
+        other = 6 - (start + end)
+        hanoi(n-1, start, other, moves)
+        moves.append((start, end))
+        hanoi(n-1, other, end, moves)
 
 def solve():
     n = int(input())
     moves = []
-    tower_of_hanoi(n, 1, 3, 2, moves)
+    hanoi(n, 1, 3, moves)
+
     print(len(moves))
     for move in moves:
         print(move[0], move[1])
