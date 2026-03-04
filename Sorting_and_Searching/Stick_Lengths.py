1eb48d925e5d2e13ccd1ba59d5f4ce4d5ff57ba3
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2026-03-04 23:48:10 +0530

    Stick_Lengths

---
+++ Sorting_and_Searching/Stick_Lengths.py
@@ -0,0 +1,11 @@
+def solve():
+    n = int(input())
+    sticks = list(map(int, input().split()))
+
+    sticks.sort()
+    median = sticks[n // 2]
+    total_cost = sum(abs(stick - median) for stick in sticks)
+    print(total_cost)
+
+if __name__ == "__main__":
+    solve()
